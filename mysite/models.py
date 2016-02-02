068a3c5cc2e473db8b96d6fadabbc812eff28db4
Stephen Sloat <stephenlsloat@gmail.com>
2016-02-02 13:30:56 -0600

    adding fb login capabilities

---
+++ mysite/models.py
@@ -0,0 +1,19 @@
+from mysite import db, lm
+
+from flask.ext.login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    social_id = db.Column(db.String(64), nullable=False, unique=True)
+    nickname = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=True)
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+if __name__ == '__main__':
+    db.create_all()
--- mysite/views.py
+++ mysite/views.py
@@ -1,11 +1,50 @@
-from . import app
+from . import app, db
+from mysite.models import User
 
-from flask import render_template
+from flask import render_template, redirect, url_for
+from flask.ext.login import login_user, logout_user, current_user
+
+from oauth import OAuthSignIn
+
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/authorize/<provider>')
+def oauth_authorize(provider):
+    if not current_user.is_anonymous:
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    return oauth.authorize()
+
+
+@app.route('/callback/<provider>')
+def oauth_callback(provider):
+    if not current_user.is_anonymous:
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    social_id, username, email = oauth.callback()
+    if social_id is None:
+        flash('Authentication failed.')
+        return redirect(url_for('index'))
+    user = User.query.filter_by(social_id=social_id).first()
+    if not user:
+        user = User(social_id=social_id, nickname=username, email=email)
+        db.session.add(user)
+        db.session.commit()
+    login_user(user, True)
+    return redirect(url_for('index'))
+
+
