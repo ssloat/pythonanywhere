db1469d457cd157bc527b7b9fba5208ffe68eec7
Stephen Sloat <stephenlsloat@gmail.com>
2016-05-10 22:50:37 -0500

    first step in switching to blueprints

--- mysite/__init__.py
+++ mysite/__init__.py
@@ -1,24 +1,41 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 
-app = Flask(__name__)
-Bootstrap(app)
-app.config.from_object('config')
-
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 lm = LoginManager()
-lm.init_app(app)
-lm.login_view = 'login'
+lm.login_view = 'user.login'
 
-import mysite.views
-import mysite.vanguard.models
-import mysite.vanguard.views
-import mysite.user.models
-import mysite.user.views
-import mysite.stocks.models
-import mysite.stocks.views
-import mysite.budget.models
-import mysite.budget.views
+#import mysite.vanguard.models
+#import mysite.vanguard.views
+#import mysite.stocks.models
+#import mysite.stocks.views
+import user.models
+import user.views
+import budget.models
+import budget.views
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config')
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
+
+
+    db.init_app(app)
+    lm.init_app(app)
+
+    app.register_blueprint(budget.views.budget_bp)
+    app.register_blueprint(user.views.user_bp)
+
+    Bootstrap(app)
+
+    return app
