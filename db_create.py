618be1adfa91e786ddeb4fa6785761f980d2ee8f
Stephen Sloat <stephenlsloat@gmail.com>
2016-06-15 22:28:16 -0500

    create db

---
+++ db_create.py
@@ -0,0 +1,20 @@
+#!flask/bin/python
+from config import SQLALCHEMY_DATABASE_URI
+from mysite import db
+
+from flask import Flask
+
+import user.models
+import investments.vanguard.models
+import investments.stocks.models
+import investments.assets.models
+import investments.portfolio.models
+import finances.budget.models
+import finances.transaction.models
+
+app = Flask(__name__)
+app.config.from_object('config')
+db.init_app(app)
+ 
+with app.app_context():
+    db.create_all()
