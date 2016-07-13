ef8f1409a1f8503bd30c2c6d132a07fa414920e2
Stephen Sloat <stephenlsloat@gmail.com>
2016-07-13 14:02:47 -0500

    update assets tables bc mysql views are bad - cant use merge algorithm with union

--- investments/assets/models.py
+++ investments/assets/models.py
@@ -8,6 +8,19 @@ import itertools
 
 logger = logging.getLogger(__name__)
 
+"""
+DEFINER VIEW `assets` AS 
+select 
+   `vanguard_funds`.`fund_type` AS `fund_type`,`vanguard_funds`.`asset_class` AS `asset_class`,
+   `vanguard_funds`.`category` AS `category`,`vanguard_funds`.`name` AS `name`,
+   `vanguard_funds`.`ticker` AS `ticker` 
+from `vanguard_funds` 
+union select 
+   'Stock' AS `fund_type`,`stocks`.`sector` AS `asset_class`,`stocks`.`industry` AS `category`,
+   `stocks`.`name` AS `name`,`stocks`.`ticker` AS `ticker` 
+from `stocks` 
+"""
+
 class Asset(db.Model):
     __tablename__ = 'assets'
 
@@ -30,13 +43,29 @@ class Asset(db.Model):
         )
         return prices[-1].close if prices else self.prices[-1]
 
+"""
+create view asset_prices as
+select concat('STOCK-', sp.id) as id, s.ticker as ticker, sp.date as date, sp.close as close,
+   sp.open_ as open_, sp.high as high, sp.low as low, sp.volume as volume
+from stocks s, stock_prices sp where sp.stock_id = s.id
+union
+select concat('VANGUARD-', vp.id) as id, vf.ticker as ticker, vp.date as date, vp.price as close,
+   null as open_, null as high, null as low, null as volume
+from vanguard_funds vf, vanguard_prices vp where vp.fund_id=vf.id
+"""
+
 class AssetPrice(db.Model):
     __tablename__ = "asset_prices"
 
-    id = db.Column(db.String(64), primary_key=True)
+    #id = db.Column(db.String(64), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     ticker = db.Column(db.String(64), db.ForeignKey('assets.ticker'))
     date = db.Column(db.Date, nullable=False)
     close = db.Column(db.Float, nullable=False)
+    open_ = db.Column(db.Float, nullable=True)
+    high = db.Column(db.Float, nullable=True)
+    low = db.Column(db.Float, nullable=True)
+    volume = db.Column(db.Float, nullable=True)
 
     def __repr__(self):
         return "<AssetPrice(%d, '%s', '%s', %f)>" % (
@@ -53,7 +82,8 @@ class AssetPrice(db.Model):
 class AssetDividend(db.Model):
     __tablename__ = "asset_dividends"
 
-    id = db.Column(db.String(64), primary_key=True)
+    #id = db.Column(db.String(64), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     ticker = db.Column(db.String(64), db.ForeignKey('assets.ticker'))
     date = db.Column(db.Date, nullable=False)
     value = db.Column(db.Float, nullable=False)
