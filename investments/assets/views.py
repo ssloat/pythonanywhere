f186fe8a0ad8a75f022735c3e7c9f4e4e3080352
Stephen Sloat <stephenlsloat@gmail.com>
2016-07-13 18:22:09 -0500

    change import

--- investments/assets/views.py
+++ investments/assets/views.py
@@ -2,19 +2,19 @@ from mysite import db
 
 from flask import Blueprint, jsonify
 
-from investments.assets import models
+from investments.assets.models import Asset
 
 asset_bp = Blueprint('asset', __name__)
 
 @asset_bp.route('/rest/asset/v1.0/list', methods=['GET'])
 def rest_assets():
-    funds = db.session.query(models.Asset).all()
+    funds = db.session.query(Asset).all()
     return jsonify({'assets': [x.json() for x in funds]})
 
 @asset_bp.route('/rest/asset/v1.0/prices/<ticker>', methods=['GET'])
 def rest_asset_prices(ticker):
-    asset = db.session.query(models.Asset).filter(
-        models.Asset.ticker==ticker
+    asset = db.session.query(Asset).filter(
+        Asset.ticker==ticker
     ).first()
 
     return jsonify({'prices': [x.json() for x in asset.prices]})
