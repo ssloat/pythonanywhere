1ce164bc5d0e3dd7b262c8474bb77f6152ed1804
Stephen Sloat <stephenlsloat@gmail.com>
2017-01-28 12:52:16 -0600

    categories urls should be under finances.  require user for new category

--- finances/views/category.py
+++ finances/views/category.py
@@ -3,23 +3,42 @@ from finances.models.category import Category, categoriesSelectBox
 from finances.forms.category import NewCategoryForm
 
 from flask import Blueprint, jsonify, render_template, request, redirect, url_for
-from flask.ext.login import current_user, login_required
+from flask_login import current_user, login_required
 
 import json
 
-category_bp = Blueprint('category', __name__, 
+category_bp = Blueprint(
+    'category', 
+    __name__, 
     template_folder='../templates',
     static_folder='../static',
     static_url_path='/static/finances',
 )
 
 
-@category_bp.route('/rest/categories', methods=['GET'])
+@category_bp.route('/finances/categories', methods=['GET', 'POST'])
+@login_required
+def categories():
+    form = NewCategoryForm(request.form)
+
+    cats = dict([(c.id, c) for c in db.session.query(Category).all()])
+    form.parent.choices = categoriesSelectBox()
+    if request.method == 'POST' and form.validate_on_submit():
+        form.process_input(current_user.id, cats)
+        form.parent.choices = categoriesSelectBox()
+
+    return render_template('categories.html', form=form)
+
+
+
+@category_bp.route('/rest/finances/categories', methods=['GET'])
+@login_required 
 def rest_categories():
     top = db.session.query(Category).filter(Category.name=='top').first()
     return jsonify({ 'tree': [top.tree()] })
 
-@category_bp.route('/rest/categories', methods=['POST'])
+@category_bp.route('/rest/finances/modify_category', methods=['POST'])
+@login_required 
 def rest_modify_category():
     json = request.form
 
@@ -33,18 +52,5 @@ def rest_modify_category():
     db.session.commit()
 
     return jsonify({})
-
-
-@category_bp.route('/categories', methods=['GET', 'POST'])
-def categories():
-    form = NewCategoryForm(request.form)
-
-    cats = dict([(c.id, c) for c in db.session.query(Category).all()])
-    form.parent.choices = [(c.id, c.name) for c in cats.values()]
-
-    if request.method == 'POST' and form.validate_on_submit():
-        form.process_input(cats)
-
-    return render_template('categories.html', form=form)
 
 
